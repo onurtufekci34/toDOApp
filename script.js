cd1a8aa5b347ddcb3d63423622981c9ece5e4340
Onur Tüfekçi <69247292+onurtufekci34@users.noreply.github.com>
2021-03-19 19:30:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-// UI vars
+// UI vars 
 
 const form = document.querySelector('form');
 const input = document.querySelector('#txtTaskName');
 const btnDeleteAll = document.querySelector('#btnDeleteAll');
 const taskList = document.querySelector('#task-list');
+const items = ['item 1', 'item 2', 'item 3', 'item 4'];
 
-// Call event listeners
+// load items
+loadItems();
+
+// call event listeners
 eventListeners();
 
-function eventListeners(){
+function eventListeners() {
     // submit event
-    form.addEventListener('submit',addNewItem);
-};      
+    form.addEventListener('submit', addNewItem);
 
-// add new item
-function addNewItem(e){
+    // delete an item
+    taskList.addEventListener('click', deleteItem);
 
-    if(input.value ===''){
-        alert('add new item');
-    }
+    // delete all items
+    btnDeleteAll.addEventListener('click', deleteAllItems);
+}
+
+function loadItems() {
+    items.forEach(function (item) {
+        createItem(item);
+    })
+}
+
+function createItem(text) {
     // create li
     const li = document.createElement('li');
+    li.className = 'list-group-item list-group-item-secondary';
+    li.appendChild(document.createTextNode(text));
 
-    li.className= 'list-group-item list-group-item-secondary';
-    li.appendChild(document.createTextNode(input.value));
     // create a
     const a = document.createElement('a');
-    a.classList='delete-item float-right';
-    a.setAttribute('href','#');
-    a.innerHTML='<i class="fas fa-times"></i>';
+    a.classList = 'delete-item float-right';
+    a.setAttribute('href', '#');
+    a.innerHTML = '<i class="fas fa-times"></i>';
 
     // add a to li
     li.appendChild(a);
@@ -36,11 +47,44 @@ function addNewItem(e){
     // add li to ul
     taskList.appendChild(li);
 
-    console.log(li);
+}
+
+// add new item
+function addNewItem(e) {
+    if (input.value === '') {
+        alert('add new item');
+    }
+
+    // create item
+    createItem(input.value);
 
     // clear input
-    input.value='';
+    input.value = '';
 
+    e.preventDefault();
 
+}
+
+// delete an item
+function deleteItem(e) {
+    if (e.target.className === 'fas fa-times') {
+        if (confirm('are you sure ?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+    e.preventDefault();
+}
+
+// delete all items
+function deleteAllItems(e) {
+
+    if (confirm('are you sure ?')) {
+        // taskList.innerHTML='';
+        taskList.childNodes.forEach(function (item) {
+            if (item.nodeType === 1) {
+                item.remove();
+            }
+        });
+    }
     e.preventDefault();
 }
